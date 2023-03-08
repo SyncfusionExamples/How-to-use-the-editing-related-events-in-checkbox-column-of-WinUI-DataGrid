7128431ec9b78f0aa43cb5ac70b15b28d1b5f137
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2023-03-08 19:18:17 +0530

    Modify the View model code changes
    Modify the View model code changes

--- SfDataGridDemo/ViewModel/ViewModel.cs
+++ SfDataGridDemo/ViewModel/ViewModel.cs
@@ -21,15 +21,15 @@ namespace DataGridDemo
 
         private void GenerateOrders()
         {
-            _orders.Add(new OrderInfo(1001, "Ana Trujilo", "Mexico", "ANATR", "Mexico D.F.",89));
-            _orders.Add(new OrderInfo(1003, "Antonio Moreno", "Mexico", "ANTON", "Mexico D.F.", 54));
-            _orders.Add(new OrderInfo(1004, "Thomas Hardy", "UK", "AROUT", "London", 32));
-            _orders.Add(new OrderInfo(1005, "Christina Berglund", "Sweden", "BERGS", "Lula", 12));
-            _orders.Add(new OrderInfo(1006, "Hanna Moos", "Germany", "BLAUS", "Mannheim",99));
-            _orders.Add(new OrderInfo(1007, "Frederique Citeaux", "France", "BLONP", "Strasbourg", 67));
-            _orders.Add(new OrderInfo(1008, "Martin Sommer", "Spain", "BOLID", "Madrid", 89));
-            _orders.Add(new OrderInfo(1009, "Laurence Lebihan", "France", "BONAP", "Marseille", 39));
-            _orders.Add(new OrderInfo(1010, "Elizabeth Lincoln", "Canada", "BOTTM", "Tsawassen", 22));
+            _orders.Add(new OrderInfo(1001, "Ana Trujilo", "Mexico", "ANATR", "Mexico D.F.",89, true));
+            _orders.Add(new OrderInfo(1003, "Antonio Moreno", "Mexico", "ANTON", "Mexico D.F.", 54, false));
+            _orders.Add(new OrderInfo(1004, "Thomas Hardy", "UK", "AROUT", "London", 32, true));
+            _orders.Add(new OrderInfo(1005, "Christina Berglund", "Sweden", "BERGS", "Lula", 12, false));
+            _orders.Add(new OrderInfo(1006, "Hanna Moos", "Germany", "BLAUS", "Mannheim",99, false));
+            _orders.Add(new OrderInfo(1007, "Frederique Citeaux", "France", "BLONP", "Strasbourg", 67, true));
+            _orders.Add(new OrderInfo(1008, "Martin Sommer", "Spain", "BOLID", "Madrid", 89, false));
+            _orders.Add(new OrderInfo(1009, "Laurence Lebihan", "France", "BONAP", "Marseille", 39, true));
+            _orders.Add(new OrderInfo(1010, "Elizabeth Lincoln", "Canada", "BOTTM", "Tsawassen", 22, false));
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
